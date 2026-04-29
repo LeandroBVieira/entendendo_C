b9aaaa532f811b60c9a0165cbadf183e59fb47d6
LeandroBVieira <leandrovieira3243@gmail.com>
2026-04-29 10:06:08 -0300

    aula 006

---
+++ 006/scan.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    int vlr1, vlr2;
+
+    printf("Digite um Número: ");
+    scanf("%d", &vlr1);
+    printf("%d", vlr1);
+
+
+    return 0;
+}
