0d8e6dd94b26a1ab8d38ed1a52e01a025f22ffdd
LeandroBVieira <leandrovieira3243@gmail.com>
2026-04-29 10:10:42 -0300

    aula 006 melhorada

--- 006/scan.c
+++ 006/scan.c
@@ -3,12 +3,24 @@
 
 int main(void) {
 
-    int vlr1, vlr2;
+    int vlr1, vlr2, vlr3;
 
-    printf("Digite um Número: ");
+    // Valor 1
+    printf("Digite o valor1: ");
     scanf("%d", &vlr1);
-    printf("%d", vlr1);
 
+    // Valor 2
+    printf("Digite o valor2: ");
+    scanf("%d", &vlr2);
+
+    // Valor 3
+    printf("Digite o valor3: ");
+    scanf("%d", &vlr3);
+
+    // Valores serão Imprimidos:
+    printf("%d", vlr1);
+    printf("%d", vlr2);
+    printf("%d", vlr3);
 
     return 0;
 }
