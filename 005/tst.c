802501cf9be8664a4144350a6f598c502053fc67
LeandroBVieira <leandrovieira3243@gmail.com>
2026-05-01 22:26:40 -0300

    aula 005 nova e atualizada

--- 005/tst.c
+++ 005/tst.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
 int main (void) {
-    int num = 12.5;
-    printf("print: %d \n", num);
+    // Feito para entender as casas decimais, tanto no float quanto no double:
+    // Normalmente mostra Seis(6) casas decimais;
+
+    float num = 12.5;
+    // três casas
+    printf("print: %.3f \n", num);
+    // Duas casas
+    printf("print: %.2f \n", num);
+    // Uma casa
+    printf("print: %.1f \n", num);
+    // Casas Normais que vem juntas com o tipo Float;
+    printf("print: %f \n", num);
 
     return 0;
 }
