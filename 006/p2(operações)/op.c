75dfd7c0e6310d4bd5037724254d401e831046cf
LeandroBVieira <leandrovieira3243@gmail.com>
2026-05-01 23:29:42 -0300

    parte 02 da aula 006 adicionada

---
+++ 006/p2(operações)/op.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+    // Feito para entender as Operações aritméticas feitas no C;
+    int num1, num2;
+    int soma, subtracao, produto,divisao, resto;
+
+    num1 = 10;
+    num2 = 5;
+
+    soma = num1 + num2;
+    subtracao = num1 - num2;
+    produto = num1 * num2;
+    divisao = num1 / num2;
+    resto = num1 % num2;
+
+    // Imprimir:
+    printf("soma: %d \n", soma);
+    printf("subtração: %d \n", subtracao);
+    printf("produto: %d \n", produto);
+    printf("divisão: %d \n", divisao);
+    printf("resto divisao: %d \n", resto);
+
+
+    return 0;
+}
