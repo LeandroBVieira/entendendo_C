40f6788af3ab98981012cee89d361a9bd9764dd4
LeandroBVieira <leandrovieira3243@gmail.com>
2026-05-02 00:22:01 -0300

    Erros feitos propositalmente para testes

--- Testes/tst1/tst1.c
+++ Testes/tst1/tst1.c
@@ -2,5 +2,14 @@
 #include <stdio.h>
 
 int main (void) {
-    
+    // TESTE FEITO PARA DAR ERRO, JÁ IREI AVISAR DE ANTEMÃO!
+    int n1 = 12;
+    printf("n1: %d \n", n1);
+
+    printf("n1: %lf \n", n1);
+    // irá dar erro aqui
+    printf("n1: %f \n", n1);
+    // irá dar erro aqui
+
+    return 0;
 };
