03658071d6e6da61622c3def5eb7ee48615f8ae1
LeandroBVieira <leandrovieira3243@gmail.com>
2026-05-09 00:38:37 -0300

    ex04 adicionado p1

---
+++ Exercicios/ex04/segundograu.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+    // Feito para fazer um exercicio de segundo grau usando pow e sqrt da biblioteca math.h
+    // sqrt -> raiz quadrada
+    // pow -> potenciação
+
+int main (void) {
+    // Entrada de Dados:
+    double a, b, c, delta, x, x1, x2;
+    a = 1;
+    b = -5;
+    c = 6;
+
+    // Processamento de Dados:
+    delta = (pow(b, 2)) - 4 * a * c;
+    // printf("%.0lf \n", delta); ignore isto, feito um teste aqui!;
+    // Saída de Dados:
+};
