c38aa3da581f1f31218f170be96d5ede5906454c
LeandroBVieira <leandrovieira3243@gmail.com>
2026-09-06 23:48:55 -0300

    mensagem de boas vindas

--- Testes/tst2/tst2.c
+++ Testes/tst2/tst2.c
@@ -3,3 +3,9 @@
 int main (void) {
     printf("Olá, Mundo!! \n");
 };
+
+/*
+que Loucura ksks, faz tempo que não entro aqui, rotina é pesada no Giassi skks, mas é bom voltar ao local na qual você gosta muito
+de ver voce progredindo, melhorando na programação e tantos outros aspectos qeu nem consegue ver, mas ás vezes é bom sair das telas e ver a natureza, conversar com Deus e socializar, por que
+não se vive só de informática, mas vive-se também de Vida ;)
+*/
