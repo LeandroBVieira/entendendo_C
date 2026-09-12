9229a68d1cd5d652d379d3f38575a3c9c2b433bc
LeandroBVieira <leandrovieira3243@gmail.com>
2026-09-11 23:46:42 -0300

    alteração de palavra

--- Testes/tst2/tst2.c
+++ Testes/tst2/tst2.c
@@ -6,6 +6,6 @@ int main (void) {
 
 /*
 que Loucura ksks, faz tempo que não entro aqui, rotina é pesada no Giassi skks, mas é bom voltar ao local na qual você gosta muito
-de ver voce progredindo, melhorando na programação e tantos outros aspectos qeu nem consegue ver, mas ás vezes é bom sair das telas e ver a natureza, conversar com Deus e socializar, por que
+de ver voce progredindo, melhorando na programação e tantos outros aspectos que nem consegue ver, mas ás vezes é bom sair das telas e ver a natureza, conversar com Deus e socializar, por que
 não se vive só de informática, mas vive-se também de Vida ;)
 */
