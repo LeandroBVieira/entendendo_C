7602e2ca77ca17f22ad0dc19d67da93ff74bcc13
LeandroBVieira <leandrovieira3243@gmail.com>
2026-09-18 23:57:16 -0300

    calc.c refeito

--- MiniProjetos/Calculadora/calc.c
+++ MiniProjetos/Calculadora/calc.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main (void) {
     // Mini calculadora feita para testar meus conhecimentos;
     // Calcula todos os resultados possíveis;
-    int num1, num2;
+    double num1, num2;
 
     // Primeiro Número:
     printf("Digite um Número: \n");
-    scanf("%d", &num1);
+    scanf("%lf", &num1);
 
     // Segundo Número:
     printf("Digite outro Número: \n");
-    scanf("%d", &num2);
+    scanf("%lf", &num2);
 
     // Imprimir os resultados possíveis:
-    printf("a soma entre %d e %d é: %.1d \n", num1, num2, num1 + num2);
-    printf("a subtração entre %d e %d é: %.1d \n", num1, num2, num1 - num2);
-    printf("o produto entre %d e %d é: %.1d \n", num1, num2, num1 * num2);
-    printf("a divisão entre %d e %d é: %.1d \n", num1, num2, num1 / num2);
-    printf("o resto da divisão entre %d e %d é: %.1d \n", num1, num2, num1 % num2);
+    printf("a soma entre %.1lf e %.1lf é: %.1lf \n", num1, num2, num1 + num2);
+    printf("a subtração entre %.1lf e %.1lf é: %.1lf \n", num1, num2, num1 - num2);
+    printf("o produto entre %.1lf e %.1lf é: %.1lf \n", num1, num2, num1 * num2);
+    printf("a divisão entre %.1lf e %.1lf é: %.1lf \n", num1, num2, num1 / num2);
+    printf("o resto da divisão entre %.1lf e %.1lf é: %.1lf \n", num1, num2, fmod(num1, num2));
 
     return 0;
 }
